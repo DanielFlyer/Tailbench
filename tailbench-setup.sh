8ac4b4e91746a3e8d5e21923adab6aa0ffaeaf05
Daniel Flyer <dflyer@princeton.edu>
2024-03-04 03:33:11 +0000

    modified setup script

--- tailbench-setup.sh
+++ tailbench-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-WORKDIR=$PWD
-TAILBENCHDIR=${WORKDIR}/tailbench
-
+#WORKDIR=$PWD
+#TAILBENCHDIR=${WORKDIR}/tailbench
+#
 if [[ ! -d ${WORKDIR}/scratch ]]; then
 	mkdir ${WORKDIR}/scratch
 fi
@@ -35,101 +35,51 @@ cd release/
 cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=/usr/bin/g++ CMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_TBB=ON -DBUILD_NEW_PYTHON_SUPPORT=ON -DWITH_V4L=ON -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON -DBUILD_EXAMPLES=ON -DWITH_QT=ON -DWITH_OPENGL=ON -DBUILD_FAT_JAVA_LIB=ON -DINSTALL_TO_MANGLED_PATHS=ON -DINSTALL_CREATE_DISTRIB=ON -DINSTALL_TESTS=ON -DENABLE_FAST_MATH=ON -DWITH_IMAGEIO=ON -DBUILD_SHARED_LIBS=OFF -DWITH_GSTREAMER=ON ..
 sudo make all -j$(nproc)
 sudo make install
+#
+#cd ~
+#pip3 install developer-tools numpy matplotlib pandas scipy
+#
+#echo -e "\033[92mSetting up environment variables\033[0m"
+#
+#cd ${TAILBENCHDIR}
+#sh ./env.sh
+#
+#cd ~
+#echo "\033[0;32mConfiguring Sphinx\033[0m\n"
+#cp -r ${TAILBENCHDIR}/sphinx ./sphinx
+#cd sphinx/
+#
+#if [[ ! -d sphinxbase-5prealpha ]]; then
+#	tar -xf sphinxbase-5prealpha
+#fi
+#
+#cd ./sphinxbase-5prealpha/
+#sudo ./autogen.sh && sudo ./configure && sudo make -j$(nproc) && sudo make install
+#
+#cd ..
+#if [[ ! -d pocketsphinx-5prealpha ]]; then
+#	tar -xf pocketsphinx-5prealpha
+#fi
+#
+#cd ./pocketsphinx-5prealpha/
+#sudo ./configure && sudo make -j$(nproc) && sudo make install
+#
+#echo -e "\033[92mConfiguring Xapian\033[0m"
+#cd ~
+#cp -r ${TAILBENCHDIR}/xapian ./xapian
+#cd ./xapian
+#
+#if [[ ! -d xapian-core-1.2.13 ]]; then
+#	tar -xf xapian-core-1.2.13
+#fi
+#
+#cd xapian-core-1.2.13
+#sudo ./configure
+#sed -i 's/CXX = g++/CXX = g++ -std=c++03/g' Makefile
+#sudo make -j$(nproc) && sudo make install
+#cd ..	
+#
+#cd ~
+#cd ${TAILBENCHDIR}
+#echo "JDK_PATH=/usr/lib/jvm/java-8-openjdk-amd64" > ./Makefile.config
 
-cd ~
-pip3 install developer-tools numpy matplotlib pandas scipy
-
-echo -e "\033[92mSetting up environment variables\033[0m"
-
-cd ${TAILBENCHDIR}
-sh ./env.sh
-
-cd ~
-echo "\033[0;32mConfiguring Sphinx\033[0m\n"
-cp -r ${TAILBENCHDIR}/sphinx ./sphinx
-cd sphinx/
-
-if [[ ! -d sphinxbase-5prealpha ]]; then
-	tar -xf sphinxbase-5prealpha
-fi
-
-cd ./sphinxbase-5prealpha/
-sudo ./autogen.sh && sudo ./configure && sudo make -j$(nproc) && sudo make install
-
-cd ..
-if [[ ! -d pocketsphinx-5prealpha ]]; then
-	tar -xf pocketsphinx-5prealpha
-fi
-
-cd ./pocketsphinx-5prealpha/
-sudo ./configure && sudo make -j$(nproc) && sudo make install
-
-echo -e "\033[92mConfiguring Xapian\033[0m"
-cd ~
-cp -r ${TAILBENCHDIR}/xapian ./xapian
-cd ./xapian
-
-if [[ ! -d xapian-core-1.2.13 ]]; then
-	tar -xf xapian-core-1.2.13
-fi
-
-cd xapian-core-1.2.13
-sudo ./configure
-sed -i 's/CXX = g++/CXX = g++ -std=c++03/g' Makefile
-sudo make -j$(nproc) && sudo make install
-cd ..	
-
-cd ~
-cd ${TAILBENCHDIR}
-echo "JDK_PATH=/usr/lib/jvm/java-8-openjdk-amd64" > ./Makefile.config
-
-echo -e "\033[92mBuilding harness\033[0m"
-cd ./harness
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding img-dnn\033[0m"
-cd ./img-dnn
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding masstree\033[0m"
-cd ./masstree
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding moses\033[0m"
-cd ./moses
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding shore\033[0m"
-cd ./shore
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding silo\033[0m"
-cd ./silo
-
-cd ./masstree
-if [[ ! -f config.h ]]; then
-	sudo ./configure
-fi
-cd ..
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding specjbb\033[0m"
-cd ./specjbb
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding sphinx\033[0m"
-cd ./sphinx
-./build.sh
-cd ..
-
-echo -e "\033[92mBuilding xapian\033[0m"
-cd ./xapian
-./build.sh
-cd ..
